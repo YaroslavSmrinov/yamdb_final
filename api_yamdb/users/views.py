7f6e2ad9cb3b8271eb6de3622466aae1ffd3f64b
Iaroslav Smirnov <yar_smir@mail.ru>
2023-03-26 21:29:48 +0300

    fix wokflow

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -4,6 +4,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework_simplejwt.views import TokenViewBase
+
 from users.permissions import IsAdmin
 from users.serializers import (TokenObtainSerializer, User, UserSerializer,
                                UserSignUpSerializer)
